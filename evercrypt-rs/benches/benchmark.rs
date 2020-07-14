01ca163fc37486f11bca0463f90b54773d69904f
raphaelrobert <raphael@wire.com>
2020-07-14 11:47:31 +0200

    Update to criterion 0.3

--- evercrypt-rs/benches/benchmark.rs
+++ evercrypt-rs/benches/benchmark.rs
@@ -3,18 +3,13 @@ extern crate criterion;
 extern crate evercrypt;
 extern crate rand;
 
-use criterion::Criterion;
-
-const DATA: &[u8; 1024] = &[1u8; 1024];
+use criterion::{BatchSize, Criterion};
 
 pub fn randombytes(n: usize) -> Vec<u8> {
     use rand::rngs::OsRng;
     use rand::RngCore;
 
-    let mut bytes = Vec::with_capacity(n);
-    for _ in 0..n {
-        bytes.push(0);
-    }
+    let mut bytes = vec![0u8; n];
     OsRng.fill_bytes(&mut bytes);
     bytes
 }
@@ -28,34 +23,52 @@ pub fn hex_to_bytes(hex: &str) -> Vec<u8> {
     bytes
 }
 
-// Crypto
-
 fn criterion_digest(c: &mut Criterion) {
     use evercrypt::digest::{Digest, Mode};
     c.bench_function("SHA1", |b| {
-        b.iter(|| {
-            let _d = Digest::hash(Mode::Sha1, DATA);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _d = Digest::hash(Mode::Sha1, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("SHA224", |b| {
-        b.iter(|| {
-            let _d = Digest::hash(Mode::Sha224, DATA);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _d = Digest::hash(Mode::Sha224, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("SHA256", |b| {
-        b.iter(|| {
-            let _d = Digest::hash(Mode::Sha256, DATA);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _d = Digest::hash(Mode::Sha256, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("SHA384", |b| {
-        b.iter(|| {
-            let _d = Digest::hash(Mode::Sha384, DATA);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _d = Digest::hash(Mode::Sha384, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("SHA512", |b| {
-        b.iter(|| {
-            let _d = Digest::hash(Mode::Sha512, DATA);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _d = Digest::hash(Mode::Sha512, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -64,16 +77,22 @@ fn criterion_aead(c: &mut Criterion) {
 
     c.bench_function("AES128 encrypt", |b| {
         let key = &randombytes(16);
-        let nonce = &randombytes(12);
-        let data = &randombytes(1_000);
-        let aad = &randombytes(1_000);
-        let aead = Aead::new(Mode::Aes128Gcm, key).unwrap();
-        b.iter(|| {
-            let (_ct, _tag) = aead.encrypt(data, &nonce, aad).unwrap();
-        });
+        b.iter_batched(
+            || {
+                let nonce = randombytes(12);
+                let data = randombytes(1_000);
+                let aad = randombytes(1_000);
+                let aead = Aead::new(Mode::Aes128Gcm, key).unwrap();
+                (data, nonce, aad, aead)
+            },
+            |(data, nonce, aad, aead)| {
+                let (_ct, _tag) = aead.encrypt(&data, &nonce, &aad).unwrap();
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("AES128 decrypt", |b| {
-        b.iter_with_setup(
+        b.iter_batched(
             || {
                 let key = randombytes(16);
                 let nonce = randombytes(12);
@@ -87,21 +106,28 @@ fn criterion_aead(c: &mut Criterion) {
                 let aead = Aead::new(Mode::Aes128Gcm, &key).unwrap();
                 let _decrypted = aead.decrypt(&ct, &tag, &nonce, &aad).unwrap();
             },
+            BatchSize::SmallInput,
         )
     });
 
     c.bench_function("AES256 encrypt", |b| {
         let key = &randombytes(32);
-        let nonce = &randombytes(12);
-        let data = &randombytes(1_000);
-        let aad = &randombytes(1_000);
-        let aead = Aead::new(Mode::Aes256Gcm, key).unwrap();
-        b.iter(|| {
-            let (_ct, _tag) = aead.encrypt(data, &nonce, aad).unwrap();
-        });
+        b.iter_batched(
+            || {
+                let nonce = randombytes(12);
+                let data = randombytes(1_000);
+                let aad = randombytes(1_000);
+                let aead = Aead::new(Mode::Aes256Gcm, key).unwrap();
+                (data, nonce, aad, aead)
+            },
+            |(data, nonce, aad, aead)| {
+                let (_ct, _tag) = aead.encrypt(&data, &nonce, &aad).unwrap();
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("AES256 decrypt", |b| {
-        b.iter_with_setup(
+        b.iter_batched(
             || {
                 let key = randombytes(32);
                 let nonce = randombytes(12);
@@ -115,21 +141,28 @@ fn criterion_aead(c: &mut Criterion) {
                 let aead = Aead::new(Mode::Aes256Gcm, &key).unwrap();
                 let _decrypted = aead.decrypt(&ct, &tag, &nonce, &aad).unwrap();
             },
+            BatchSize::SmallInput,
         )
     });
 
     c.bench_function("ChaCha20Poly1305 encrypt", |b| {
         let key = &randombytes(32);
-        let nonce = &randombytes(12);
-        let data = &randombytes(1_000);
-        let aad = &randombytes(1_000);
-        let aead = Aead::new(Mode::Chacha20Poly1305, key).unwrap();
-        b.iter(|| {
-            let (_ct, _tag) = aead.encrypt(data, &nonce, aad).unwrap();
-        });
+        b.iter_batched(
+            || {
+                let nonce = randombytes(12);
+                let data = randombytes(1_000);
+                let aad = randombytes(1_000);
+                let aead = Aead::new(Mode::Chacha20Poly1305, key).unwrap();
+                (data, nonce, aad, aead)
+            },
+            |(data, nonce, aad, aead)| {
+                let (_ct, _tag) = aead.encrypt(&data, &nonce, &aad).unwrap();
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("ChaCha20Poly1305 decrypt", |b| {
-        b.iter_with_setup(
+        b.iter_batched(
             || {
                 let key = randombytes(32);
                 let nonce = randombytes(12);
@@ -143,6 +176,7 @@ fn criterion_aead(c: &mut Criterion) {
                 let aead = Aead::new(Mode::Chacha20Poly1305, &key).unwrap();
                 let _decrypted = aead.decrypt(&ct, &tag, &nonce, &aad).unwrap();
             },
+            BatchSize::SmallInput,
         )
     });
 }
@@ -150,27 +184,36 @@ fn criterion_aead(c: &mut Criterion) {
 fn criterion_x25519(c: &mut Criterion) {
     use evercrypt::x25519::{x25519, x25519_base};
     c.bench_function("X25519 base", |b| {
-        let sk = &randombytes(32);
-        b.iter(|| {
-            let _pk = x25519_base(sk);
-        });
+        b.iter_batched(
+            || randombytes(32),
+            |sk| {
+                let _pk = x25519_base(&sk);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("X25519 DH", |b| {
-        let sk1 = &randombytes(32);
-        let pk1 = x25519_base(sk1);
-        let sk2 = &randombytes(32);
-        b.iter(|| {
-            let _zz = x25519(&pk1, sk2).unwrap();
-        });
+        b.iter_batched(
+            || {
+                let sk1 = randombytes(32);
+                let pk1 = x25519_base(&sk1);
+                let sk2 = randombytes(32);
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let _zz = x25519(&pk1, &sk2).unwrap();
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
 fn criterion_p256(c: &mut Criterion) {
     use evercrypt::p256::{p256_dh, p256_dh_base};
 
-    const PK1_HEX:&str = "0462d5bd3372af75fe85a040715d0f502428e07046868b0bfdfa61d731afe44f26ac333a93a9e70a81cd5a95b5bf8d13990eb741c8c38872b4a07d275a014e30cf";
+    const PK1_HEX: &str = "0462d5bd3372af75fe85a040715d0f502428e07046868b0bfdfa61d731afe44f26ac333a93a9e70a81cd5a95b5bf8d13990eb741c8c38872b4a07d275a014e30cf";
     const SK1_HEX: &str = "0612465c89a023ab17855b0a6bcebfd3febb53aef84138647b5352e02c10c346";
-    const _PK2_HEX:&str = "04bd07bd4326cdcabf42905efa4559a30e68cb215d40c9afb60ce02d4fda617579b927b5cba02d24fb9aafe1d429351e48bae9dd92d7bc7be15e5b8a30a86be13d";
+    const _PK2_HEX: &str = "04bd07bd4326cdcabf42905efa4559a30e68cb215d40c9afb60ce02d4fda617579b927b5cba02d24fb9aafe1d429351e48bae9dd92d7bc7be15e5b8a30a86be13d";
     const SK2_HEX: &str = "00809c461d8b39163537ff8f5ef5b977e4cdb980e70e38a7ee0b37cc876729e9ff";
 
     c.bench_function("P256 base", |b| {
@@ -191,25 +234,41 @@ fn criterion_p256(c: &mut Criterion) {
 fn criterion_ed25519(c: &mut Criterion) {
     use evercrypt::ed25519::{ed25519_sign, ed25519_sk2pk, ed25519_verify};
     c.bench_function("ed25519 key gen", |b| {
-        let sk = &randombytes(32);
-        b.iter(|| {
-            let _pk = ed25519_sk2pk(sk);
-        });
+        b.iter_batched(
+            || randombytes(32),
+            |sk| {
+                let _pk = ed25519_sk2pk(&sk);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("ed25519 sign", |b| {
-        let sk1 = &randombytes(32);
-        let pk1 = ed25519_sk2pk(sk1);
-        b.iter(|| {
-            let _sig = ed25519_sign(&pk1, DATA);
-        });
+        b.iter_batched(
+            || {
+                let sk = randombytes(32);
+                let data = randombytes(1_000);
+                (sk, data)
+            },
+            |(sk, data)| {
+                let _sig = ed25519_sign(&sk, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("ed25519 verify", |b| {
-        let sk1 = &randombytes(32);
-        let pk1 = ed25519_sk2pk(sk1);
-        let sig = ed25519_sign(&pk1, DATA);
-        b.iter(|| {
-            let _valid = ed25519_verify(&pk1, &sig, DATA);
-        });
+        b.iter_batched(
+            || {
+                let sk = randombytes(32);
+                let pk = ed25519_sk2pk(&sk);
+                let data = randombytes(1_000);
+                let sig = ed25519_sign(&pk, &data);
+                (pk, data, sig)
+            },
+            |(pk, data, sig)| {
+                let _valid = ed25519_verify(&pk, &sig, &data);
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -217,24 +276,40 @@ fn criterion_hmac(c: &mut Criterion) {
     use evercrypt::hmac::{hmac, Mode};
     const KEY: [u8; 10] = [0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     c.bench_function("HMAC SHA1", |b| {
-        b.iter(|| {
-            let _hmac = hmac(Mode::Sha1, &KEY, DATA, None);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _hmac = hmac(Mode::Sha1, &KEY, &data, None);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HMAC SHA256", |b| {
-        b.iter(|| {
-            let _hmac = hmac(Mode::Sha256, &KEY, DATA, None);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _hmac = hmac(Mode::Sha256, &KEY, &data, None);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HMAC SHA384", |b| {
-        b.iter(|| {
-            let _hmac = hmac(Mode::Sha384, &KEY, DATA, None);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _hmac = hmac(Mode::Sha384, &KEY, &data, None);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HMAC SHA512", |b| {
-        b.iter(|| {
-            let _hmac = hmac(Mode::Sha512, &KEY, DATA, None);
-        });
+        b.iter_batched(
+            || randombytes(1_000),
+            |data| {
+                let _hmac = hmac(Mode::Sha512, &KEY, &data, None);
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -272,73 +347,129 @@ fn criterion_hkdf(c: &mut Criterion) {
     });
 
     c.bench_function("HKDF expand SHA1", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        let prk = hkdf_extract(Mode::Sha1, &salt, &ikm);
-        b.iter(|| {
-            let _okm = hkdf_expand(Mode::Sha1, &prk, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let prk = hkdf_extract(Mode::Sha1, &salt, &ikm);
+                let data = randombytes(1_000);
+                (len, prk, data)
+            },
+            |(len, prk, data)| {
+                let _okm = hkdf_expand(Mode::Sha1, &prk, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF expand SHA256", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        let prk = hkdf_extract(Mode::Sha256, &salt, &ikm);
-        b.iter(|| {
-            let _okm = hkdf_expand(Mode::Sha256, &prk, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let prk = hkdf_extract(Mode::Sha1, &salt, &ikm);
+                let data = randombytes(1_000);
+                (len, prk, data)
+            },
+            |(len, prk, data)| {
+                let _okm = hkdf_expand(Mode::Sha256, &prk, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF expand SHA384", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        let prk = hkdf_extract(Mode::Sha384, &salt, &ikm);
-        b.iter(|| {
-            let _okm = hkdf_expand(Mode::Sha384, &prk, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let prk = hkdf_extract(Mode::Sha1, &salt, &ikm);
+                let data = randombytes(1_000);
+                (len, prk, data)
+            },
+            |(len, prk, data)| {
+                let _okm = hkdf_expand(Mode::Sha384, &prk, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF expand SHA512", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        let prk = hkdf_extract(Mode::Sha512, &salt, &ikm);
-        b.iter(|| {
-            let _okm = hkdf_expand(Mode::Sha512, &prk, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let prk = hkdf_extract(Mode::Sha1, &salt, &ikm);
+                let data = randombytes(1_000);
+                (len, prk, data)
+            },
+            |(len, prk, data)| {
+                let _okm = hkdf_expand(Mode::Sha512, &prk, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
 
     c.bench_function("HKDF SHA1", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        b.iter(|| {
-            let _hkdf = hkdf(Mode::Sha1, &salt, &ikm, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let data = randombytes(1_000);
+                (ikm, salt, len, data)
+            },
+            |(ikm, salt, len, data)| {
+                let _hkdf = hkdf(Mode::Sha1, &salt, &ikm, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF SHA256", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        b.iter(|| {
-            let _hkdf = hkdf(Mode::Sha256, &salt, &ikm, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let data = randombytes(1_000);
+                (ikm, salt, len, data)
+            },
+            |(ikm, salt, len, data)| {
+                let _hkdf = hkdf(Mode::Sha256, &salt, &ikm, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF SHA384", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        b.iter(|| {
-            let _hkdf = hkdf(Mode::Sha384, &salt, &ikm, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let data = randombytes(1_000);
+                (ikm, salt, len, data)
+            },
+            |(ikm, salt, len, data)| {
+                let _hkdf = hkdf(Mode::Sha384, &salt, &ikm, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("HKDF SHA512", |b| {
-        let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
-        let salt = hex_to_bytes("000102030405060708090a0b0c");
-        let len = 32;
-        b.iter(|| {
-            let _hkdf = hkdf(Mode::Sha512, &salt, &ikm, DATA, len);
-        });
+        b.iter_batched(
+            || {
+                let ikm = hex_to_bytes("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+                let salt = hex_to_bytes("000102030405060708090a0b0c");
+                let len = 32;
+                let data = randombytes(1_000);
+                (ikm, salt, len, data)
+            },
+            |(ikm, salt, len, data)| {
+                let _hkdf = hkdf(Mode::Sha512, &salt, &ikm, &data, len);
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
