eeb73e8b95d40640d87a60a0f0a420259023e82d
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-10-26 16:36:00 +0100

    really fix it this time

--- evercrypt-rs/src/p256.rs
+++ evercrypt-rs/src/p256.rs
@@ -34,7 +34,7 @@ fn validate_pk(pk: &[u8]) -> Result<[u8; 64], Error> {
         }
     }
     let valid = unsafe { Hacl_P256_verify_q(public.as_ptr() as _) };
-    if (!uncompressed_point && !compressed_point) || !valid {
+    if !uncompressed_point && !compressed_point && !valid {
         return Err(Error::InvalidPoint);
     }
 
