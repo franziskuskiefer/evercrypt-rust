17cb5b81e6b96650dcadc679431f7e1071f4f95b
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2021-03-11 12:22:57 +0000

    deploy: 1f12d23099ed642ead437d7b653132904fabd4dd

--- evercrypt/rand_util/sidebar-items.js
+++ evercrypt/rand_util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_random_array","Generate a random array."],["get_random_vec","Generate a random byte vector of length `len`."]]});
+initSidebarItems({"fn":[["get_random_array","Generate a random array. PANICS if randomness generation fails."],["get_random_vec","Generate a random byte vector of length `len`. PANICS if randomness generation fails."]]});
