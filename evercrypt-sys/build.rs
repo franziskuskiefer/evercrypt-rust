f895dd7ea81691efeb15b5bc06d97d8e00124fd2
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-05-17 09:24:03 +0200

    ios target

--- evercrypt-sys/build.rs
+++ evercrypt-sys/build.rs
@@ -306,6 +306,8 @@ fn main() {
     let build_config = match target.as_str() {
         "x86_64-apple-darwin" => cfg.set_cross_config_flags(vec!["-target", "x86_64-apple-darwin"]),
         "aarch64-apple-darwin" => cfg.set_cross_config_flags(vec!["-target", "aarch64-apple-darwin"]),
+        "aarch64-apple-ios" => cfg.set_cross_config_flags(vec!["-target", "aarch64-apple-ios"]),
+        // "x86_64-apple-ios" => cfg.set_cross_config_flags(vec!["-target", "x86_64-apple-ios-simulator"]),
         "i686-unknown-linux-gnu" => cfg.set_cross_config_flags(vec!["-target", "ia32"]),
         "x86_64-unknown-linux-gnu" => {
             cfg.set_cross_config_flags(vec!["-target", "x86_64-unknown-linux-gnu"])
